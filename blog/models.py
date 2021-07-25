5e0afd7b1fdbfe055db727b925b395b341ef754b
mailzahidul <mailzahidul@gmail.com>
2021-07-25 12:27:57 +0600

    Navbar

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_name
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
+from .models import Category
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'pages/home.html')
+    categories = Category.objects.all()
+    print(categories)
+    context = {
+        'categories': categories
+    }
+    return render(request, 'pages/home.html', context)
+
+
+# def category_view(request):
+#     # categories = Category.objects.all()
+#     # print(categories)
+#     context = {
+#         'categories': categories
+#     }
+#     return render(request, 'components/header.html', context)
